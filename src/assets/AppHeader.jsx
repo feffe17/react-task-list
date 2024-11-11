51758e6c636f90d099fb31cc0731f16312c40c22
feffe1702 <fpm.asd@gmail.com>
2024-11-11 19:57:10 +0100

    [WIP] complete header

---
+++ src/assets/AppHeader.jsx
@@ -0,0 +1,11 @@
+export default function AppHeader() {
+
+    const appName = "Task Manager";
+
+    return (
+        <header>
+            <h1>{appName}</h1>
+        </header>
+    )
+
+}
---
+++ src/assets/AppMain.jsx
@@ -0,0 +1,10 @@
+export default function AppMain() {
+
+    const intro = "welcome to my react app"
+    return (
+        <main>
+            <div>{intro}</div>
+        </main>
+    )
+
+}
